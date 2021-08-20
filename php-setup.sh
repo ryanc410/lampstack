e8d99bf75ea9092efa285a4ea356017f4835c63b
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-08-20 16:31:07 -0500

    08/20/2021
    Fixed quotations on commands without user variables

--- php-setup.sh
+++ php-setup.sh
@@ -121,17 +121,17 @@ sed -i "s|;date.timezone =|date.timezone = $tz|g" /etc/php/7.4/fpm/php.ini
 sed -i "s|;date.timezone =|date.timezone = $tz|g" /etc/php/7.4/cli/php.ini
 sed -i "s|;date.timezone =|date.timezone = $tz|g" /etc/php/7.4/apache2/php.ini
 
-sed -i "s|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/fpm/php.ini
-sed -i "s|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/cli/php.ini
-sed -i "s|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/apache2/php.ini
+sed -i 's|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/fpm/php.ini
+sed -i 's|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/cli/php.ini
+sed -i 's|;sendmail_path =|sendmail_path = /usr/sbin/sendmail|g' /etc/php/7.4/apache2/php.ini
 
-sed -i "s|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/fpm/php.ini
-sed -i "s|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/cli/php.ini
-sed -i "s|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/apache2/php.ini
+sed -i 's|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/fpm/php.ini
+sed -i 's|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/cli/php.ini
+sed -i 's|mysqli.default_socket =|mysqli.default_socket = /var/run/mysqld/mysqld.sock|g' /etc/php/7.4/apache2/php.ini
 
-sed -i "s/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/fpm/php.ini
-sed -i "s/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/cli/php.ini
-sed -i "s/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/apache2/php.ini
+sed -i 's/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/cli/php.ini
+sed -i 's/mysqli.default_host =/mysqli.default_host = localhost/g' /etc/php/7.4/apache2/php.ini
 
 sed -i "s/mysqli.default_user =/mysqli.default_user = $mysql_user/g" /etc/php/7.4/fpm/php.ini
 sed -i "s/mysqli.default_user =/mysqli.default_user = $mysql_user/g" /etc/php/7.4/cli/php.ini
