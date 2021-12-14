b44d984d04120222fb14a3deb9e0df6450d64dc7
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-12-14 17:58:49 -0600

    12-14-2021
    Updated script

--- composer-install.sh
+++ composer-install.sh
@@ -1,55 +1,66 @@
-#!/bin/bash
-#
-# Author: Ryan Cook
-# Date: 08/24/2021
-# Description: Installs Composer quickly
-
-check_root()
+#!/usr/bin/env bash
+########################################################################
+# Author: Ryan C. https://github.com/ryanc410
+# Date: 08-24-2021
+# Version: 2.0
+# Description: Installs Composer PHP package manager 
+########################################################################
+# VARIABLES
+########################################################################
+ret_val="$?"
+HASH=$(curl -sS https://composer.github.io/installer.sig)
+########################################################################
+# FUNCTIONS
+########################################################################
+function check_os()
 {
-    if [[ $EUID -ne 0 ]]; then
-        clear
-        echo "Must be root to run this script!"
-        sleep 2
-        exit 1
+    cat /etc/lsb-release | grep 20 &>/dev/null
+    if [[ $ret_val != 0 ]]; then
+        sudo apt install software-properties-common -y
+        sudo add-apt-repository ppa:ondrej/php -y
+        sudo apt update
+        sudo apt install php7.4 -y
+    else
+        sudo apt install php7.4 -y
     fi
 }
-check_os()
-{
-    if [[ $OSTYPE -ne linux-gnu ]]; then
-        clear
-        echo "Script not compatible with your OS.."
-        sleep 2
-        exit 1
-    fi
-}
-title()
-{
-    clear
-    echo "############################"
-    echo "#    Composer Installer    #"
-    echo "############################"
-}
+########################################################################
+# SCRIPT
+########################################################################
+sudo apt update && sudo apt upgrade -y
+php -v &>/dev/null
 
-check_os
-
-check_root
-
-
-echo "Updating system.."
-apt update &>/dev/null
-
-echo "Installing required components.."
-apt install php-cli unzip -y &>/dev/null
-
-command -v curl &>/dev/null
-if [[ $? == 1 ]]; then
-    apt install curl -y &>/dev/null
+if [[ $retval != 0 ]]; then
+    echo "ERROR: Composer needs PHP to function.. Do you want to install PHP now?"
+    read opt
+    case "$opt" in
+        y|Y|yes|Yes|YES)
+            check_os
+            ;;
+        n|N|no|No|NO)
+            echo "Exiting script.."
+            sleep 2
+            exit 0
+            ;;
+        *)
+            echo "Invalid Response.."
+            exit 1
+            ;;
+    esac
 fi
 
-echo "Downloading Composer Install Script.."
-curl -sS https://getcomposer.org/installer -o composer-setup.php &>/dev/null
+dpkg -l | grep php-cli &>/dev/null
+if [[ $ret_val != 0 ]]; then
+    sudo apt install php-cli -y
+fi
 
-echo "Installing.."
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer &>/dev/null
+dpkg -l | grep unzip &>/dev/null
+if [[ $ret_val != 0 ]]; then
+    sudo apt install unzip -y
+fi
 
-echo "Installation Complete!"
+cd ~
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+[ -f /usr/local/bin/composer ] && echo "Composer Successfully Installed!" || echo "Installation failed!"
